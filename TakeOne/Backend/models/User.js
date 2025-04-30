d39acd32e03022ff9f177d678341844f87ab77b9
Dasari Saideep <saideepdasari30@gmail.com>
2025-04-30 13:26:52 +0530

    db relationship

--- TakeOne/Backend/models/User.js
+++ TakeOne/Backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 // 📦 MongoDB Schema for User collection
+// Implemented DB entity relationships
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
